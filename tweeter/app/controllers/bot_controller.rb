a64bcbb4aadc865e42fdc46680f3f432d20a957d
aurbanski <aurbanski27@gmail.com>
2017-03-10 13:15:02 -0700

    Changed the root

--- tweeter/app/controllers/bot_controller.rb
+++ tweeter/app/controllers/bot_controller.rb
@@ -1,10 +1,7 @@
 require 'twitter'
 
 class BotController < ApplicationController
-  @client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['consumer_key']
-    config.consumer_secret     = ENV['consumer_secret']
-    config.access_token        = ENV['access_token']
-    config.access_token_secret = ENV['access_secret']
+  def show
+    render html: "hello"
   end
 end
